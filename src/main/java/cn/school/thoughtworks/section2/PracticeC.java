07aa124c10f0c4ae3aa732cc1a2960356a350df6
chenmanru75 <chenmanru75@163.com>
2019-05-14 20:26:27 +0800

    section2-practiceC ok

--- src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -1,12 +1,33 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String, Integer> res = new HashMap<>();
+        for(String str:collection1){
+            if(str.length() == 1){
+                Integer num = res.get(str);
+                res.put(str, num == null ? 1: num+1);
+            }else{
+                Integer num = res.get(Character.toString(str.charAt(0)));
+                if(str.charAt(1) == '-'){
+                    String[] buff = str.split("-");
+                    res.put(buff[0], num == null ? Integer.parseInt(buff[1]) : Integer.parseInt(buff[1]) + num);
+                }
+                if(str.charAt(1) == ':'){
+                    String[] buff = str.split(":");
+                    res.put(buff[0], num == null ? Integer.parseInt(buff[1]) : Integer.parseInt(buff[1]) + num);
+                }
+                if(str.charAt(1) == '['){
+                    String buff = str.substring(2,str.length()-1);
+                    String[] buff2 = str.split("\\[");
+                    res.put(buff2[0], num == null ? Integer.parseInt(buff) : Integer.parseInt(buff) + num);
+                }
+            }
+        }
+        return res;
     }
 }
